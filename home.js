051c684834a21b516667de0bdda3d6e0e1d469cd
MD RUMEL AHMED <rumelrumel0987654321@gmail.com>
2026-03-07 03:00:01 +0600

    complete btn toggling

---
+++ home.js
@@ -0,0 +1,78 @@
+const totalIssue = document.getElementById("total-issue")
+const allBtns = document.querySelectorAll("#btnsDiv button");
+
+allBtns.forEach((btn, index) => {
+  if(index === 0){
+    btn.classList.add("btn-primary");
+    btn.classList.remove("btn-outline");
+  } else {
+    btn.classList.add("btn-outline");
+    btn.classList.remove("btn-primary");
+  }
+});
+
+allBtns.forEach(btn => {
+  btn.addEventListener("click", function() {
+    allBtns.forEach(b => {
+      b.classList.remove("btn-primary");
+      b.classList.add("btn-outline");
+    });
+    btn.classList.remove("btn-outline");
+    btn.classList.add("btn-primary");
+  });
+});
+
+
+const loadAllIssue = async () => {
+    const url = 'https://phi-lab-server.vercel.app/api/v1/lab/issues'
+    const res = await fetch (url)
+    const data = await res.json()
+    displayIssue(data.data);
+    totalIssue.innerText= data.data.length + " Issue"
+}
+
+loadAllIssue()
+
+const displayIssue  = (cards) => {
+    const cardConatiner = document.getElementById("cardDiv" )
+    cardConatiner.innerHTML=""
+    cards.forEach(card => {
+    const cardDiv = document.createElement("div")
+    cardDiv.className="bg-gray-100 p-2 space-y-4 py-4 rounded-sm"
+    cardDiv.innerHTML=`
+    <div class="space-y-4">
+        <div class="flex justify-between">
+         <img src="./assets/Open-Status.png" >
+         <div class="badge badge-md">${card.priority}</div>
+       </div>
+           <h2 class="text-lg font-semibold" >${card.title}</h2>
+         <h3 class="text-xs">${card.description}</h3>
+         <div>
+          <div class="badge badge-soft badge-primary">${card.labels}</div>
+          <div class="badge badge-soft badge-warning">Help Wanted</div>
+          <hr class="text-gray-300 mt-6">
+          <p class="text-xs mt-2">#1 ${card.author}</p>
+          <p class="text-xs mt-1.5">${card.createdAt}</p>
+          <p class="text-xs mt-1.5">${card.updatedAt}</p>
+         </div>
+     </div>
+    `
+    cardConatiner.appendChild(cardDiv)
+    })
+    
+}
+
+
+
+// "id": 9,
+// "title": "Add export to PDF feature",
+// "description": "Users want to export reports and dashboards to PDF format for sharing and printing.",
+// "status": "open",
+// "labels": [
+// "enhancement"
+// ],
+// "priority": "medium",
+// "author": "feature_fred",
+// "assignee": "",
+// "createdAt": "2024-01-16T10:15:00Z",
+// "updatedAt": "2024-01-16T10:15:00Z"
